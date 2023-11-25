21f501c543f0e419d4626f9371178837cfbfc0d5
Ilan Nestor Angeles Rodriguez <ilananagelesrodriguez@gmail.com>
2023-11-25 18:08:05 -0500

    test: agregar boton de NextUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image'
+import {Button} from "@nextui-org/react";
+
 
 export default function Home() {
   return (
@@ -39,6 +41,10 @@ export default function Home() {
         />
       </div>
 
+      <Button color="primary">
+        Button
+      </Button>
+
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -55,6 +61,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Find in-depth information about Next.js features and API.
           </p>
+
         </a>
 
         <a
