61edac56d0988331ecbfb34f6c85f89aafb6a9fc
Ilan Nestor Angeles Rodriguez <ilananagelesrodriguez@gmail.com>
2023-11-26 10:03:06 -0500

    refactor: corregir importacion de elemento

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
-import {AcmeLogo} from "./AcmeLogo.tsx";
+import {AcmeLogo} from "./AcmeLogo";
 
 export default function App() {
     return (
